6389293881aabb79c56fa8d721d27da43c3c3dc0
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-07-22 20:20:30 -0300

    feat: add remove item action

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,16 @@
+import { useDispatch } from 'react-redux'
+import { removeItem } from '../features/cart/cartSlice'
 import { ChevronDown, ChevronUp } from '../icons'
 
 const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch()
+
   return <article className='cart-item'>
     <img src={img} alt={title} />
     <div>
       <h4>{title}</h4>
       <h4 className="item-price">${price}</h4>
-      <button className='remove-btn'>Remove</button>
+      <button className='remove-btn' onClick={() => dispatch(removeItem(id))}>Remove</button>
     </div>
 
     <div>
