d160e218b2b4d9ad34c884c3a6cd77fbe8bbdac3
leovdn <leo_vdn@hotmail.com>
2022-07-28 20:46:36 -0300

    feat: get users from api with Async Thunk

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
+import { fetchUsers } from "./features/users/usersSlice"
+import { store } from "./store"
+
 import "./index.css"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
+
+store.dispatch(fetchUsers())
 
 root.render(
   <React.StrictMode>
